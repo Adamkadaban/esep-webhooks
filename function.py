51627cd3bffec6e4505f9edc3b4accb1fc4214c4
adamkadaban <adamkadaban@gmail.com>
2022-10-16 21:52:41 -0400

    added return for request response

--- function.py
+++ function.py
@@ -6,4 +6,5 @@ def lambda_handler(event, context):
 	message = "Issue Created " + event['issue']['html_url']
 	with open('slack_hook.secret') as fin:
 		url = fin.read().strip()
-	requests.post(url, json = {"text":message})
+	response = requests.post(url, json = {"text":message})
+	return response.txt
